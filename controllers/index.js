923e384cf4215895772c6b022b4d12eb90eb23aa
zxielle <@JahazielFaye>
2023-02-27 14:36:54 -0500

    update style and control file

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,13 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home.routes');
 const dashboardRoutes = require('./dashboard.routes');
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+
+function myMiddleware(req, res, next) {
+  router.use('/api', apiRoutes);
+  router.use('/', homeRoutes);
+  router.use('/dashboard', dashboardRoutes);
+    next();
+}
 
 router.use((req, res) => {
   res.status(404).end();
